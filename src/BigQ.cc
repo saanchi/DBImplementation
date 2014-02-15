1268304a04f7cb398160911ec673b36f667d430e
saanchi <saanchithefriend@gmail.com>
2014-02-15 15:08:23 -0500

    Added merge function

--- src/BigQ.cc
+++ src/BigQ.cc
@@ -2,6 +2,9 @@
 #include "Defs.h"
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <queue>
+#include <iterator>
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 
@@ -12,11 +15,9 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 	this->in  = in;
 	this->out = out;
 	this->runlen = runlen;
-	X 		  	 = new File();
 	Y 		  	 = new File();
 	this->sortorder  = sortorder;
-	output_buffer 	 = new Page<Record>();
-	run_offset.push_back(1);	// Initialize for the first run
+	run_offset.push_back(0);	// Initialize for the first run
 	pthread_create (&worker, NULL, run, (void *)this );
 }
 
@@ -24,25 +25,22 @@ BigQ::~BigQ () {
 }
 
 void * run( void *arg ){
-
 	BigQ *bq = (BigQ *) arg;
 	generate_runs(bq);   // Phase I of TPMMS
-	initialize_buffers(bq);
 	multiway_merge(bq);    // Phase II  of TPMMS
-    // finally shut down the out pipe
-	bq->out->ShutDown();
+	bq->out->ShutDown();   // finally shut down the out pipe
 }
 
 void generate_runs( BigQ *bq ){
 // read data from in pipe sort them into runlen pages
-	Record rec;
-	Page page;
-	vector<Record> buffer;
-	int count   = 0; 					// keep track of number of pages
+	Record *rec;
+	Page *page = new Page();
+	vector<Record *> buffer;
+	int count     = 0; 					// keep track of number of pages
 	off_t run_num = 0;					// keep track of number of runs
-	while( bq->in->Remove(&rec)){  		// keep reading from the pipe
+	while( bq->in->Remove(rec)){  		// keep reading from the pipe
 		buffer.push_back(rec);          // Put record in buffer
-		int status = page.Append(&buffer.back()); // Try adding the record to page
+		int status = page->Append(rec); // Try adding the record to page
 		if( status == 0){				// if page is full
 			count++;
 			page = NULL;
@@ -54,7 +52,8 @@ void generate_runs( BigQ *bq ){
 				count = 0;					     // initialize the count to 0
 			}
 			page = new Page();
-			page.Append(&rec);
+			rec  = buffer.back();
+			page->Append(rec);
 		}
 	}
 	// Sort and write the last run
@@ -62,30 +61,52 @@ void generate_runs( BigQ *bq ){
 	write_buffer( bq->Y, buffer );	 				   // write the records from buffer into a file
 }
 
-void write_buffer( File *Y, vector<Record> buffer){
-	Page page = new Page();
-	Record rec;
-	for( vector<int>::iterator itr = buffer.begin(); itr != buffer.end(); ++itr){
-		rec = buffer.pop_back();
-		int status = page.Append(&rec);
+void write_buffer( File *Y, vector<Record *> *buffer){
+	Page *page = new Page();
+	Record *rec;
+	for( vector<int>::iterator itr = buffer->begin(); itr != buffer->end(); ++itr){
+		rec = buffer->pop_back();
+		int status = page->Append(rec);
 		if( status == 0 ){ 					// if page is full
 			off_t offset = Y->GetLength();
-			Y->AddPage(&page, ++offset); 	// write the page into file
+			Y->AddPage(page, ++offset); 	// write the page into file
 			page = new Page();
 		}
 	}
 }
 
-void initialize_buffers( BigQ *bq){
-	for( vector<int>::iterator itr = bq->run_offset.begin(); itr != bq->run_offset.end(); ++itr){
-
-
-	}
-}
-
 void multiway_merge(BigQ *bq){
+	priority_queue<P*, vector<P*>, PairComparison> *pq;
+	Page *page;
+	// Initialize the buffers and the priority queue.
+	// Fetch the corresponding first pages from the File into the buffer
+	for( int i =0; i < bq->run_offset.size(); i++){
+	    bq->Y->GetPage( page, bq->run_offset[i]);		// Fetch the page
+        bq->run_buffer[i] = page;        // Put in the buffer
+        bq->run_offset[i]++;				// increase the offset of the current buffer
+        Record *rec = new Record;
+        page->GetFirst(rec);                // Fetch the first record from the page
+        P *pr = new P(rec, i);				// Create a pair
+        pq->push(pr);						// Insert the pair into priority queue
+	}
+	// Do the merge
+	while( !pq->empty()){
+		P *pr = pq->top();					// Extract the top pair from the queue
+		Record *rec = pr->first;				// Get the record
+		bq->out->Insert(rec);				// insert the record into the output pipe
+		int index  = pr->second;				// Get the buffer index
+		page  = bq->run_buffer[index];
+		if( !page->GetFirst(rec)){			// if the page is empty
+			off_t curr_run_offset = bq->run_offset[index];
+			// check if the current run's runlen pages have not been read or
+			// current run is the end run and not yet finished
+			if( curr_run_offset% bq->runlen != 0 || ( curr_run_offset != bq->Y->GetLength())){
+				page = new Page;
+				bq->Y->GetPage(page, curr_run_offset+1); // Fetch the page
+				bq->run_buffer[index] = page;			  // Place the fetched page into buffer
+				bq->run_offset[index]++;				  // increase the counter
+			}
+		}
+	}
 
 }
-
-
-
--- src/BigQ.h
+++ src/BigQ.h
@@ -8,27 +8,41 @@
 #include <queue>
 
 using namespace std;
+typedef pair<Record *, int> P;
+
+class PairComparison{
+private :
+    OrderMaker *sort_order;
+    ComparisonEngine *comparison_engine;
+public :
+    PairComparison( OrderMaker *sort_order ){
+        this->sort_order        = sort_order;
+        this->comparison_engine = new ComparisonEngine;
+    }
+    bool operator()(P const& a, P const& b) const{
+        return comparison_engine->Compare( a.first, b.first, sort_order) <= 0;
+    }
+};
 
 class BigQ {
 
 public:
 
-	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
-	~BigQ ();
+    BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
+    ~BigQ ();
 
 private:
-	pthread_t worker;		  // Only worker thread
-	queue<Record> min_queue;  // in-memory priority queue for phase II of TPMMS
-    File *X, *Y;              // Temporary files used for external sorting.
-    vector<Page> run_buffer;  // In-memory buffer for each of the 'm' runs
-    Page *output_buffer;      // Buffer to hold output from the multi-way merge.
-    Pipe *in, *out;			  // Input pipe output pipe
-    int runlen;				  // Run length of Pages to be kept in memory
-    OrderMaker *sortorder;    // Order maker object
-    vector<off_t> run_offset; // Store the offsets for the runs generated
+    pthread_t worker;            // Only worker thread
+    File *Y;                     // Temporary files used for external sorting.
+    vector<Page *> run_buffer;   // In-memory buffer for each of the 'm' runs
+    vector<off_t> run_offset;    // Store the offsets for the runs generated
+    // Objects passed as arguments to the BigQ constructor
+    Pipe *in, *out;              // Input pipe output pipe
+    int runlen;                  // Run length of Pages to be kept in memory
+    OrderMaker *sortorder;       // Order maker object
 };
 
-	/**
+    /**
      * Starting point for thread.
      * Reads data from the input pipe.
      * Sorts them using TPMMS
@@ -37,10 +51,10 @@ private:
     void * run( void *arg);
 
     /**
-	 * Read the sorted buffer into pages.
-	 * Write the pages into disk
-	 */
-	void write_buffer( File *Y, vector<Record> buffer);
+     * Read the sorted buffer into pages.
+     * Write the pages into disk
+     */
+    void write_buffer( File *Y, vector<Record *> buffer);
 
     /**
      * Implements Phase I of TPMMS.
@@ -55,17 +69,10 @@ private:
      * Initialize the in memory buffer corresponding to each run.
      * Initialize the priority queue with the head element of each run.
      * Keep extracting min from the queue and write to output buffer.
-     *	If the run gets empty load from the disk.
-     *	When the output buffer gets full write to disk.
+     *    If the run gets empty load from the disk.
+     *    When the output buffer gets full write to disk.
      * Repeat till all the elements are sorted.
      */
     void multiway_merge( BigQ *bq );
-
-    /**
-     *  Initialize the in memory buffer for each run.
-     *  Initialize the priority queue.
-     */
-    void initialize_buffers( BigQ *bq );
-
 
 #endif
